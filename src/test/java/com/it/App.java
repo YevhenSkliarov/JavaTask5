3ef59b9cc4370cac85af994fbc61b6c2c966face
YevhenSkliarov <eugensklyarov90@gmail.com>
2020-01-23 23:04:06 +0200

    remove unused imports

--- src/test/java/com/it/App.java
+++ src/test/java/com/it/App.java
@@ -6,14 +6,14 @@ public class App {
     public CommonHelper common;
     public DashboardHelper dashboard;
     public LoginHelper login;
-    public CreateCreateLetterHelper letter;
+    public CreateLetterHelper letter;
     public IncomeLetterHelper incomeLetter;
 
     public App() {
         this.common = new CommonHelper();
         this.dashboard = new DashboardHelper();
         this.login = new LoginHelper();
-        this.letter = new CreateCreateLetterHelper();
+        this.letter = new CreateLetterHelper();
         this.incomeLetter = new IncomeLetterHelper();
     }
 }
--- src/test/java/com/it/helpers/CreateLetterHelper.java
+++ src/test/java/com/it/helpers/CreateLetterHelper.java
@@ -3,7 +3,7 @@ package com.it.helpers;
 import com.it.letter.CreateLetter;
 import com.it.pages.CreateLetterPage;
 
-public class CreateCreateLetterHelper extends CreateLetterPage {
+public class CreateLetterHelper extends CreateLetterPage {
     public void sendLetter(CreateLetter createLetter) {
         sendLetter(createLetter.to, createLetter.subject, createLetter.body);
     }
