a5a34c62b654bcca83c2d726fa863136cd07d32a
YevhenSkliarov <eugensklyarov90@gmail.com>
2020-01-25 11:15:32 +0200

    add fiefox

---
+++ src/test/java/com/it/driver/DriverFactory.java
@@ -0,0 +1,26 @@
+package com.it.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+    public static WebDriver getDriver() {
+        WebDriver driver;
+        String driverSysProperty = System.getProperty("driver");
+        if ("firefox".equals(driverSysProperty)) {
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().fullscreen();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.switchTo().activeElement();//switchTo - working with iframe
+        driver.get("https://www.i.ua/");
+        return driver;
+    }
+}
--- src/test/java/com/it/pages/BasePage.java
+++ src/test/java/com/it/pages/BasePage.java
@@ -1,5 +1,6 @@
 package com.it.pages;
 
+import com.it.driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -7,17 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.concurrent.TimeUnit;
 
 public class BasePage {
-    static protected WebDriver driver;
-
-    static {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        System.out.println(driver.manage().getCookies());
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.switchTo().activeElement();//switchTo - working with iframe
-        driver.get("https://www.i.ua/");
-    }
+    static protected WebDriver driver = DriverFactory.getDriver();
 
     public BasePage() {
         PageFactory.initElements(driver,this);
