75492e435162924821365f7d8fec36ed7ed0f722
YevhenSkliarov <eugensklyarov90@gmail.com>
2020-06-15 19:37:44 +0300

    add random user

--- src/test/java/com/it/users/UserFactory.java
+++ src/test/java/com/it/users/UserFactory.java
@@ -1,6 +1,11 @@
 package com.it.users;
 
+import com.it.utils.QAUtils;
+
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserFactory {
     private static ResourceBundle resourceUser = ResourceBundle.getBundle("users");
@@ -10,7 +15,20 @@ public class UserFactory {
                 resourceUser.getString("password"),
                 resourceUser.getString("email"));
     }
-    /*public static User getRandomUser(){
+    public static User getRandomUser(){
+            return new User(
+                QAUtils.getRandomString(15),
+                QAUtils.getRandomString(10),
+                QAUtils.getRandomString(12)+"@gmail.com");
+    }
 
-    };*/
+    public  static List<User> getRandomUsers(int count) {
+        List<User> collect = Stream.generate(() -> new User(
+                QAUtils.getRandomString(15),
+                QAUtils.getRandomString(10),
+                QAUtils.getRandomString(12)+"@gmail.com"))
+                .limit(count)
+                .collect(Collectors.toList());
+        return collect;
+    }
 }
